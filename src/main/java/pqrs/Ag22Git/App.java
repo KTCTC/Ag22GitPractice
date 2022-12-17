8e4a35b58d2da9d364da27c866304696c68a0dbf
KTCTC <ktcorporatetrainingcentre@gmail.com>
2022-12-17 20:47:51 +0530

    First commit

---
+++ src/main/java/pqrs/Ag22Git/App.java
@@ -0,0 +1,13 @@
+package pqrs.Ag22Git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/pqrs/Ag22Git/AppTest.java
@@ -0,0 +1,20 @@
+package pqrs.Ag22Git;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
