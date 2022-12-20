83aa8b07c7ec9fffffa588c60af2e97ebb60ca45
KTCTC <ktcorporatetrainingcentre@gmail.com>
2022-12-20 21:22:28 +0530

    added new testcase in Brand web page

--- src/test/java/regression/BrandTest.java
+++ src/test/java/regression/BrandTest.java
@@ -5,6 +5,11 @@ public class BrandTest {
 	public void show()
 	{
 		System.out.println("Hello");
+		System.out.println("Second line by Ganesh");
+	}
+	public void sampleMethod()
+	{
+		System.out.println("This method is for future perpose");
 	}
 }
  
