95f4aab9332991cd6e5c9163196690d4485841fa
KTCTC <ktcorporatetrainingcentre@gmail.com>
2022-12-20 21:07:44 +0530

    updated Inventory Test script and Added new test cases for UnitTest

--- src/test/java/regression/InventoryTest.java
+++ src/test/java/regression/InventoryTest.java
@@ -5,6 +5,10 @@ public class InventoryTest {
 	public static void main(String[] args) {
 		System.out.println("sample code");
 		System.out.println("second line");
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(i);
+		}
 	}
 
 }
---
+++ src/test/java/regression/UnitTest.java
@@ -0,0 +1,10 @@
+package regression;
+
+public class UnitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Sample code");
+	}
+
+}
