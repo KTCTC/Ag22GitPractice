a382bf1f89da76761640e04d14c107b5821aecaf
DeepZagade <savyasachi583@gmail.com>
2022-12-20 21:00:08 +0530

    Updated files

--- src/test/java/regression/InventoryTest.java
+++ src/test/java/regression/InventoryTest.java
@@ -2,9 +2,15 @@ package regression;
 
 public class InventoryTest {
 	
+	
+	public void show()
+	{
+		System.out.println("New Method");
+	}
 	public static void main(String[] args) {
 		System.out.println("sample code");
 		System.out.println("second line");
+		System.out.println("Third Line By Deep");
 	}
 
 }
--- src/test/java/regression/ProductPage2.java
+++ src/test/java/regression/ProductPage2.java
@@ -5,5 +5,6 @@ public class ProductPage2 {
 	public void print()
 	{
 			System.out.println("Product page");
+			System.out.println("Second Line By Deep");
 	}
 }
